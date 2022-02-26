bfe80ee4c5b81035aced70a541e78bfc04887e1d
Martinus Dobbelaar <mado@netcompany.com>
2022-02-26 15:01:45 +0100

    Update .gitignore file

--- src/controllers/BookMeetingRoomController.ts
+++ src/controllers/BookMeetingRoomController.ts
@@ -4,10 +4,9 @@ import { meetingRoom, meetingRoomModel } from '../models/meetingRoomModel'
 const formatDate = (date: string) => dayjs(new Date(date)).format('YYYY-MM-DDTHH:mm:ss.SSSZ');
 
 export const getAvailableMeetingRooms = async (req: any, res: any) => {
-  console.log(req.query);
   const {startDateTime, endDateTime} = req.query;
-  // send dateTime with request
   const meetingRooms: meetingRoomModel[] = await meetingRoom.find();
+  // Below DB filtering probably did not work because of missing date formatting
 /*     {bookedTimes: 
        { $not:  
         { $elemMatch: 
@@ -27,22 +26,19 @@ export const getAvailableMeetingRooms = async (req: any, res: any) => {
 };
 
 export const getBookedTimeSlots = async (req: any, res: any) => {
-  console.log(req.params.roomId);
   const timeSlots = await meetingRoom.findById(req.params.roomId, {bookedTimes:1})
+  
   res.status(200).json(timeSlots);
 };
 
 export const setReservation = async (req: any, res: any) => {
-  console.log("make reservation");
-  console.log(req.params);
-  console.log(req.body);
   const createReservation = await meetingRoom.updateOne({ roomId: req.params.roomId}, { $push: {bookedTimes: req.body.params}}, {new: true});
-  console.log("body");
-  console.log(req.body.params);
+
   res.status(200).json(createReservation);
 };
 
 export const deleteReservation = async (req: any, res: any) => {
   await meetingRoom.remove(req.params.id);
+
   res.status(200).json({ id: req.params.id});
 };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import express from "express";
 import { connectToDatabase } from './config/db'
-//import config from "dotenv";
 require('dotenv').config();
 
 connectToDatabase();
 
 const app = express();
-const port = process.env.PORT; // 8080 // process.env.PORT; // default port to listen
-
-// ** TODO ** Replace this code with a call to your games router class to handle all calls to /games endpoint
-app.get("/", (req, res) => {
-    res.send("Hello world!");
-});
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
@@ -21,6 +15,4 @@ app.use('/api/bookMeetingRooms', require('./routes/bookMeetingRoomsRoutes'));
 // start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
-
-    // ** TODO ** Call to Game Service to initiate connection
 });
